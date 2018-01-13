bbce7f599eaf86d94790a1fe240e0b035addc5ab
arshadnazeersoftware <arshadnazeer@live.com>
2018-01-13 19:24:22 +0530

    What is a delegate?

---
+++ simpledelegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simpledelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
